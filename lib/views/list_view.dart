603b87939eaa0df29c23a05050fab7d9c8fea1f2
acolburn <acolburn3@gmail.com>
2024-04-28 16:38:07 -0700

    removed unnecessary code from list_view

--- lib/views/list_view.dart
+++ lib/views/list_view.dart
@@ -8,32 +8,29 @@ class NoteList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Using Scaffold so I can have a FAB
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: context.read<Data>().notes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            tileColor: Colors.grey[200],
-            title: Text(
-              context.read<Data>().notes[index].title,
-              maxLines: 1,
-            ),
-            subtitle: Text(
-              context.read<Data>().notes[index].content,
-              maxLines: 2,
-            ),
-            trailing: IconButton(
-                icon: Icon(YaruIcons.trash_filled),
-                onPressed: () {
-                  context.read<Data>().delete(index);
-                }),
-            onTap: () {
-              context.read<Data>().selectIndex(index);
-            },
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: context.read<Data>().notes.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          tileColor: Colors.grey[200],
+          title: Text(
+            context.read<Data>().notes[index].title,
+            maxLines: 1,
+          ),
+          subtitle: Text(
+            context.read<Data>().notes[index].content,
+            maxLines: 2,
+          ),
+          trailing: IconButton(
+              icon: Icon(YaruIcons.trash_filled),
+              onPressed: () {
+                context.read<Data>().delete(index);
+              }),
+          onTap: () {
+            context.read<Data>().selectIndex(index);
+          },
+        );
+      },
     );
   }
 }
